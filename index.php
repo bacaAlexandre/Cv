7686c6548f833a4975a3951d11406b22cc2089ca
bacaAlexandre <Alexandre76b@aol.com>
2017-02-05 13:51:04 +0100

    creation site de base html+css

---
+++ index.php
@@ -0,0 +1,146 @@
+<?php
+
+
+
+ ?>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/css/master.css">
+
+    <!-- Lien cdm pour fontAwesome -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <title></title>
+  </head>
+  <body>
+    <header>
+      <div class="header">
+        <h1>Alexandre Baca</h1>
+        <h2>Développeur/Intégrateur web</h2>
+        <nav>
+          <ul class="liste_navigation">
+            <a href="#"><li>Accueil</li></a>
+            <a href="#"><li>Expériences</li></a>
+            <a href="#"><li>Formations</li></a>
+            <a href="#"><li>Compétences</li></a>
+            <a href="#"><li>Contact</li></a>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <div class="wrapper">
+      <aside class="">
+        <img class ="photo" src="asset/images/alexandre.jpg" alt="photo d'alexandre baca">
+        <div class="info">
+          <h3>Infos</h3>
+          <ul>
+            <li>27 ans</li>
+            <li>Permis B</li>
+            <li>Hautot sur mer 76550</li>
+          </ul>
+        </div>
+        <div class="contact">
+          <h3>Contact</h3>
+          <ul>
+            <li><a href="#" class="icone iconeContact"><i class="fa fa-envelope" aria-hidden="true"></i></a></li>
+            <li><a href="#" class="icone iconeContact"><i class="fa fa-phone-square" aria-hidden="true"></i></a></li>
+          </ul>
+        </div>
+        <div class="presentation">
+          <h3>Présentation</h3>
+          <p>Dessinateur industriel reconverti en développeur web, jeune et motiver. Actuellement à la recherche d'une entreprise pour le BAC+2 d'analyse programmeur au CESI de Rouen.</p>
+        </div>
+        <div class="liens">
+          <h3>Mes liens </h3>
+          <ul>
+            <li><a href="#" class="icone"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+            <li><a href="#" class="icone"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            <li><a href="#" class="icone"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+          </ul>
+        </div>
+      </aside>
+      <main>
+        <div class="conteneur">
+          <div class="experiences">
+            <h2>Expériences</h2>
+            <article class="">
+              <h3>Dessinateur industriel</h3>
+              <p>Daher verdelet</p>
+              <p>2013 à 2014</p>
+            </article>
+            <article class="">
+              <h3>Dessinateur en construction mécanique</h3>
+              <p>Gault industries</p>
+              <p>2012</p>
+            </article>
+            <article class="">
+              <h3>Dessinateur industriel</h3>
+              <p>SODDIM</p>
+              <p>2011</p>
+            </article>
+          </div>
+          <div class="formations">
+            <h2>Formations</h2>
+            <article class="">
+              <h3>Certificats de compétence professionnelle</h3>
+              <p>Webforce3</p>
+              <p>Octobre 2016 à février 2017</p>
+              <p>Dévelopeur Web</p>
+            </article>
+            <article class="">
+              <h3>Certificats de compétence professionnelle</h3>
+              <p>Webforce3</p>
+              <p>Octobre 2016 à février 2017</p>
+              <p>Dévelopeur Web</p>
+            </article>
+            <article class="">
+              <h3>BTS</h3>
+              <p>Pablo Neruda</p>
+              <p>2010</p>
+              <p>Conception et Industrialisation Microtechnique</p>
+            </article>
+            <article class="">
+              <h3>BAC</h3>
+              <p>Pablo Neruda</p>
+              <p>2008</p>
+              <p>Industriel, Science et technique de l'Ingénieur</p>
+            </article>
+          </div>
+        </div>
+        <div class="competences">
+          <h2>Compétences</h2>
+          <article class="web">
+            <h3>Web</h3>
+            <ul>
+              <li>HTML/CSS</li>
+              <li>Javascript</li>
+              <li>JQuerry</li>
+              <li>Ajax</li>
+              <li>MySQL</li>
+              <li>PHP (php procédural et orienté objet)</li>
+              <li>MVC</li>
+              <li>Wordpress</li>
+              <li>Boostrap</li>
+            </ul>
+          </article>
+          <article class="logiciel">
+            <h3>Logiciel</h3>
+            <ul>
+              <li>Git (ligne de commade)</li>
+              <li>Git Hub</li>
+              <li>Atom</li>
+              <li>phpstorm</li>
+              <li>SolidWorks (CAO)</li>
+              <li>SolidEdge (CAO)</li>
+              <li>SAP</li>
+            </ul>
+          </article>
+        </div>
+      </main>
+    </div>
+  </body>
+</html>

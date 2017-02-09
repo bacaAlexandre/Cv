1772b3db43edcf8e9cf8b8f578a3e22cc26cac4c
bacaAlexandre <Alexandre76b@aol.com>
2017-02-09 17:21:58 +0100

    correction mail.php

--- mail.php
+++ mail.php
@@ -31,7 +31,7 @@ if(empty($_POST['nom'])){
         $message = "Email envoyer par ".$_POST['nom']."  ".$_POST['prenom']."\r\n";
         $message .= "Adress email pour repondre : ".$_POST['email']."\r\n";
         $message .= $_POST['message'];
-        var_dump($message);
+
 
         $headers  = 'MIME-Version: 1.0' . "\r\n";
         $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
